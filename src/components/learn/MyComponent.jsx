6252eb81eef7cc53efdc549a53ff44b39b9de195
Phatnt <tanphat1815@gmail.com>
2025-03-14 12:32:37 +0700

    .JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,15 @@
+// JSX -> la ten cong nghe giup dich code React (HTML + JS)
+// .jsx am chi code React voi js
 // (tham so dau vao) => { body }
+import './style.css';
+
 const MyComponent = () => {
     return(/* tat ca data ben trong la gia tri tra ve */
-      <div>tanphatnt</div>  //tag
+      // <> = fragment
+      <> 
+        <div>tanphatnt</div>  //tag
+        <div className="child"></div>
+      </>
     );
 }
 
